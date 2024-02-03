4b2cbc4903c1141fd603b00f0d01ec9b52574a18
pdosigit <37262009+pdosigit@users.noreply.github.com>
2024-02-03 10:07:26 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+def parse_json_and_add_size(file='default.json'):
+  '''
+    This function parse a json file in following format and add the fields corresponding to  "maximum_size"
+{"libs":{
+         "sweete":{
+
+                    "confir":                       {"maximum size" : 55765,  "change justification" : " initial = 55765,   "},
+                    "config_data":             {"maximum size" : 43457,  "change justification" : " initial = 43457,   "},
+                }
+          }
+  }
+ 
+
+
+  '''
